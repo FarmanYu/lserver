5908772083f78fddfd93351bc5ee6d07f5d1d134
余鸿飞 <yuhongfei@hupu.com>
2015-11-03 09:36:44 +0800

    add bin

--- index.js
+++ index.js
@@ -3,18 +3,22 @@ var program = require('commander');
 var path = require("path");
 var open = require("open");
 
-program
-    .version('0.0.1')
-    .option('-s, --server [value]', '将本地文件夹加入本地服务器')
-    .option('-p, --port [value]', '指定本地服务器端口，默认端口3000')
-    .parse(process.argv);
+module.exports = {
+	run: function(argv) {
+		program
+		    .version('0.0.1')
+		    .option('-s, --server [value]', '将本地文件夹加入本地服务器')
+		    .option('-p, --port [value]', '指定本地服务器端口，默认端口3000')
+		    .parse(argv);
 
-var localDir = path.normalize(program.server);
-var port = program.port || 3000;
-var app = express();
-app.use(express.static(localDir));
-var server = app.listen(port, function(){
-	console.log("local start: ["+ localDir +"]");
-	console.log("server start at: http://localhost:" + port);
-	open("http://localhost:" + port);
-});
+		var localDir = path.normalize(program.server);
+		var port = program.port || 3000;
+		var app = express();
+		app.use(express.static(localDir));
+		var server = app.listen(port, function(){
+			console.log("local start: ["+ localDir +"]");
+			console.log("server start at: http://localhost:" + port);
+			open("http://localhost:" + port);
+		});
+	}
+};
